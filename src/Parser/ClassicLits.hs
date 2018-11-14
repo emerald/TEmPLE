c5d6655335d2488a716a68ddf465a4454fbccf26
Oleks <oleks@oleks.info>
2018-11-14 15:56:17 +0100

    Parse numeric literals as well

--- src/Parser/ClassicLits.hs
+++ src/Parser/ClassicLits.hs
@@ -6,7 +6,14 @@ import Ast (Lit(..))
 
 import Parser.Common (token, word)
 
-import Text.ParserCombinators.ReadP (ReadP, choice)
+import Control.Applicative ((*>))
+import Data.Char (isDigit)
+import Numeric (readDec, readFloat, readHex, readOct)
+import Text.ParserCombinators.ReadP
+  ( ReadP
+  , choice, munch, satisfy, string
+  , readS_to_P
+  )
 
 parseNil :: ReadP Lit
 parseNil = word [("nil", LNil)]
@@ -17,8 +24,38 @@ parseBool = word
   , ("false", LBool False)
   ]
 
+parseHex :: ReadP Lit
+parseHex = fmap LInt $
+  string "0x" *> readS_to_P readHex
+
+parseOct :: ReadP Lit
+parseOct = fmap LInt $
+  string "0" *> readS_to_P readOct
+
+parseIntFloat :: ReadP Lit
+parseIntFloat = do
+  first <- satisfy (`elem` ['1'..'9'])
+  rest <- munch isDigit
+  let integral = first:rest
+  choice
+    [ return $ (LInt . fst . head) $ readDec integral
+    , do
+      string "."
+      fractional <- munch isDigit
+      let float = integral ++ "." ++ fractional
+      return $ (LDouble . fst . head) $ readFloat float
+    ]
+
+parseNum :: ReadP Lit
+parseNum = choice
+  [ parseIntFloat
+  , parseHex
+  , parseOct
+  ]
+
 parseLit :: ReadP Lit
 parseLit = token $ choice
   [ parseNil
   , parseBool
+  , parseNum
   ]
