7d653553c93f422fe010a5c8438d1d493418e203
Oleks <oleks@oleks.info>
2018-11-14 16:01:19 +0100

    Allow parentheses around expressions

--- src/Parser/ClassicExprs.hs
+++ src/Parser/ClassicExprs.hs
@@ -3,14 +3,15 @@ module Parser.ClassicExprs
   ) where
 
 import Ast (Expr(..))
-import Parser.Common (token)
+import Parser.Common (token, stoken)
 import Parser.ClassicNames (parseName)
 import Parser.ClassicLits (parseLit)
 
-import Text.ParserCombinators.ReadP (ReadP, choice)
+import Text.ParserCombinators.ReadP (ReadP, between, choice)
 
 parseExpr :: ReadP Expr
 parseExpr = token $ choice
   [ fmap ELit parseLit
   , fmap EVar parseName
+  , between (stoken "(") (stoken ")") parseExpr
   ]
