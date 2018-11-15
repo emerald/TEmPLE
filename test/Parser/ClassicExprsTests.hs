aa6193c2464148670806279498329ef92de3d43e
Oleks <oleks@oleks.info>
2018-11-15 15:11:26 +0100

    Parse full text to make sure invalids fail

--- test/Parser/ClassicExprsTests.hs
+++ test/Parser/ClassicExprsTests.hs
@@ -1,6 +1,6 @@
 module Parser.ClassicExprsTests (testTree) where
 
-import Parser.Common (parse)
+import Parser.Common (fullParse, parse)
 import Parser.ClassicExprs (parseExpr)
 
 import Parser.GenClassicExprs (ValidExpr(..), InvalidExpr(..))
@@ -9,10 +9,12 @@ import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.QuickCheck (Property, (===), testProperty)
 
 prop_validExpr :: ValidExpr -> Property
-prop_validExpr (ValidExpr (s, e)) = parse parseExpr s === [(e, "")]
+prop_validExpr (ValidExpr (s, e))
+  = parse parseExpr s === [(e, "")]
 
 prop_invalidExpr :: InvalidExpr -> Property
-prop_invalidExpr (InvalidExpr s) = parse parseExpr s === []
+prop_invalidExpr (InvalidExpr s)
+  = fullParse parseExpr s === []
 
 testTree :: IO TestTree
 testTree = fmap (testGroup "ClassicExprsTests") $ sequence
