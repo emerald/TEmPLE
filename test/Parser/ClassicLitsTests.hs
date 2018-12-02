f6340bbc3a72655f92137b0c3747279301a54b01
Oleks <oleks@oleks.info>
2018-12-02 12:48:38 +0100

    Test some obviously invalid lits explicitly

--- test/Parser/ClassicLitsTests.hs
+++ test/Parser/ClassicLitsTests.hs
@@ -5,8 +5,23 @@ import Parser.ClassicLits (parseLit)
 
 import Parser.GenClassicLits (ValidLit(..), InvalidLit(..))
 
+import Control.Monad (forM_)
 import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.Hspec (Spec, it, shouldBe, testSpec)
 import Test.Tasty.QuickCheck (Property, (===), testProperty)
+import Text.Printf (printf)
+
+invalidLits :: [String]
+invalidLits =
+  [ "09"
+  , "0xx"
+  ]
+
+spec_invalidLits :: Spec
+spec_invalidLits = do
+  forM_ invalidLits $ \ lit ->
+    it (printf "%s is an invalid literal" lit) $
+      fullParse parseLit lit `shouldBe` []
 
 prop_validLit :: ValidLit -> Property
 prop_validLit (ValidLit (s, e))
@@ -18,7 +33,8 @@ prop_invalidLit (InvalidLit s)
 
 testTree :: IO TestTree
 testTree = fmap (testGroup "ClassicLitsTests") $ sequence
-  [ return $ testProperty
+  [ testSpec "InvalidLits" spec_invalidLits
+  , return $ testProperty
       "Valid lits parse"
       prop_validLit
   , return $ testProperty
