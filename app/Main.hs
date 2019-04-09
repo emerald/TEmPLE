507224dd17162edfaab41e61e8319268cc368f35
Oleks <oleks@oleks.info>
2019-04-09 16:09:55 +0200

    Actually do something in main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,54 @@
 module Main where
 
-import Parser
+import Ast (Compilation)
+import Parser (parseFile, ParseError)
+
+import Data.List (intercalate)
+import System.Environment (getArgs)
+import System.Exit (exitWith, ExitCode (ExitFailure))
+import System.IO (hPutStrLn, stderr)
+
+errReport :: String -> IO ()
+errReport = hPutStrLn stderr
+
+failReport :: String -> IO a
+failReport s = errReport s >> exitWith (ExitFailure 1)
+
+parseError :: ParseError -> IO a
+parseError e = failReport $ show e
+
+parseFile' :: FilePath -> IO Compilation
+parseFile' path = do
+  result <- parseFile path
+  case result of
+    Right compilation -> pure compilation
+    Left e -> parseError e
+
+parseFiles :: [FilePath] -> IO [Compilation]
+parseFiles = mapM parseFile'
+
+noCommand :: IO a
+noCommand = failReport "Tell me what to do!"
+
+invalidCommand :: [String] -> IO a
+invalidCommand s = do
+  case s of
+    [] -> return ()
+    [x] -> failReport $
+            x ++ " is an invalid command-line argument!"
+    xs -> failReport $
+            (intercalate " " xs) ++
+              " are invalid command-line arguments!"
+  exitWith (ExitFailure 1)
 
 main :: IO ()
-main = return ()
+main = do
+  args <- getArgs
+  case args of
+    [] -> noCommand
+    ("parse" : paths) ->
+      with paths $ putStrLn . show
+    l -> invalidCommand l
+  where
+    with :: [FilePath] -> ([Compilation] -> IO ()) -> IO ()
+    with paths f = parseFiles paths >>= f
