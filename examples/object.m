e8892db2f752c0774b8d701c24995883e7e52f9a
Oleks <oleks@oleks.info>
2019-07-24 15:38:42 +0200

    More elaborate object examples

---
+++ examples/object.m
@@ -0,0 +1,11 @@
+const x <- object x
+  const xy <- object xy
+    const z : integer <- 4
+  end xy
+end x
+
+const y <- object y
+  const yy <- object yy
+    const z : integer <- 4
+  end yy
+end y
