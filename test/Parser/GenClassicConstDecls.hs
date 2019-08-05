b40b7a2585def23fcc279dda3bfea1f602c84aab
Oleks <oleks@oleks.info>
2019-08-05 11:31:00 +0200

    Separate out generation of the const keyword

--- test/Parser/GenClassicConstDecls.hs
+++ test/Parser/GenClassicConstDecls.hs
@@ -3,27 +3,14 @@ module Parser.GenClassicConstDecls
   , InvalidConstDecl(..)
   ) where
 
-import Ast (ConstDecl(..), Type)
+import Ast ( ConstDecl(..) )
 
-import Parser.GenCommon
-  ( invalidOp, invalidOp1
-  , token, token1
-  , validOp, validOp1
-  )
-import Parser.GenClassicIdents
-  ( ValidIdent(..), InvalidIdent(..)
-  , validIdentString
-  )
-import Parser.GenClassicTypes (ValidType(..), InvalidType(..))
-import Parser.GenClassicExprs
-  ( ValidExpr(..), InvalidExpr(..)
-  , validExprString
-  )
+import Parser.GenCommon ( invalidOp1, validOp1 )
+import Parser.GenClassicDecls( ValidDecl(..), InvalidDecl(..) )
 
-import Control.Applicative ((<*>))
 import Test.Tasty.QuickCheck
   ( Arbitrary, Gen
-  , arbitrary, choose, oneof, shuffle, suchThat
+  , arbitrary, choose, frequency
   )
 
 newtype ValidConstDecl
@@ -39,50 +26,25 @@ validConst = validOp1 constOp
 invalidConst :: Gen String
 invalidConst = invalidOp1 constOp
 
-assOp :: String
-assOp = "<-"
-
-validAss :: Gen String
-validAss = validOp assOp
-
-invalidAss :: Gen String
-invalidAss = invalidOp assOp
-
-validMaybeType :: Gen (String, Maybe Type)
-validMaybeType = oneof
-  [ return ("", Nothing)
-  , do
-    (st, t) <- fmap validType arbitrary
-    return (st, Just t)
-  ]
-
 instance Arbitrary ValidConstDecl where
   arbitrary = fmap ValidConstDecl $ do
     sc      <- validConst
-    (sn, n, _) <- fmap validIdent arbitrary
-    (st, t) <- validMaybeType
-    sass    <- validAss
-    (se, e, _) <- fmap validExpr arbitrary
-    return (sc ++ sn ++ st ++ sass ++ se, Const n t e)
+    (ValidDecl (sd, (n, t, e))) <- arbitrary
+    return (sc ++ sd, Const n t e)
 
 newtype InvalidConstDecl
   = InvalidConstDecl { invalidConstDecl :: String }
   deriving (Eq, Show)
 
-validInvalid :: [(Gen String, Gen String)]
-validInvalid =
-  [ (validConst                     , invalidConst)
-  , (fmap validIdentString arbitrary , fmap invalidIdent arbitrary)
-  , (fmap fst validMaybeType        , fmap invalidType arbitrary)
-  , (validAss                       , invalidAss)
-  , (fmap validExprString arbitrary , fmap invalidExpr arbitrary)
-  ]
-
 instance Arbitrary InvalidConstDecl where
-  arbitrary = fmap InvalidConstDecl $ do
-    let n = length validInvalid
-    n_fst <- choose (1, n)
-    let n_snd = n - n_fst
-    fs <- shuffle $ replicate n_fst fst ++ replicate n_snd snd
-    let gens = fs <*> validInvalid
-    fmap concat $ sequence gens
+  arbitrary = fmap InvalidConstDecl $
+    frequency [(20, do
+                      sc <- invalidConst
+                      (ValidDecl (sd, _)) <- arbitrary
+                      return $ sc ++ sd
+               ),
+               (80, do
+                      sc <- validConst
+                      (InvalidDecl sd) <- arbitrary
+                      return $ sc ++ sd
+               )]
---
+++ test/Parser/GenClassicDecls.hs
@@ -0,0 +1,74 @@
+module Parser.GenClassicDecls
+  ( ValidDecl(..)
+  , InvalidDecl(..)
+  ) where
+
+import Ast ( Expr, Ident, Type )
+
+import Parser.GenCommon ( invalidOp, validOp )
+import Parser.GenClassicIdents
+  ( ValidIdent(..), InvalidIdent(..)
+  , validIdentString
+  )
+import Parser.GenClassicTypes ( ValidType(..), InvalidType(..) )
+import Parser.GenClassicExprs
+  ( ValidExpr(..), InvalidExpr(..)
+  , validExprString
+  )
+
+import Control.Applicative ( (<*>) )
+import Test.Tasty.QuickCheck
+  ( Arbitrary, Gen
+  , arbitrary, choose, oneof, shuffle, suchThat
+  )
+
+newtype ValidDecl
+  = ValidDecl
+  { validDecl :: (String, (Ident, Maybe Type, Expr)) }
+  deriving (Eq, Show)
+
+assOp :: String
+assOp = "<-"
+
+validAss :: Gen String
+validAss = validOp assOp
+
+invalidAss :: Gen String
+invalidAss = invalidOp assOp
+
+validMaybeType :: Gen (String, Maybe Type)
+validMaybeType = oneof
+  [ return ("", Nothing)
+  , do
+    (st, t) <- fmap validType arbitrary
+    return (st, Just t)
+  ]
+
+instance Arbitrary ValidDecl where
+  arbitrary = fmap ValidDecl $ do
+    (sn, n, _) <- fmap validIdent arbitrary
+    (st, t) <- validMaybeType
+    sass    <- validAss
+    (se, e, _) <- fmap validExpr arbitrary
+    return (sn ++ st ++ sass ++ se, (n, t, e))
+
+newtype InvalidDecl
+  = InvalidDecl { invalidDecl :: String }
+  deriving (Eq, Show)
+
+validInvalid :: [(Gen String, Gen String)]
+validInvalid =
+  [ (fmap validIdentString arbitrary  , fmap invalidIdent arbitrary)
+  , (fmap fst validMaybeType          , fmap invalidType arbitrary)
+  , (validAss                         , invalidAss)
+  , (fmap validExprString arbitrary   , fmap invalidExpr arbitrary)
+  ]
+
+instance Arbitrary InvalidDecl where
+  arbitrary = fmap InvalidDecl $ do
+    let n = length validInvalid
+    n_fst <- choose (1, n)
+    let n_snd = n - n_fst
+    fs <- shuffle $ replicate n_fst fst ++ replicate n_snd snd
+    let gens = fs <*> validInvalid
+    fmap concat $ sequence gens
