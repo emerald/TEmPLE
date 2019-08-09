f32eb426abd686bcacb2caf6d5a93d237d933d98
Oleks <oleks@oleks.info>
2019-08-10 00:05:32 +0200

    Generate binary expressions as well

--- test/Parser/Classic/Gen/Exprs.hs
+++ test/Parser/Classic/Gen/Exprs.hs
@@ -7,15 +7,16 @@ module Parser.Classic.Gen.Exprs
 import Ast (Expr(..))
 import Parser.Common (fullParse)
 import Parser.Classic.Lits (parseLit)
+import Parser.Classic.Exprs (prec8, prec7, prec6, prec5)
 
-import Parser.GenCommon (token)
+import Parser.GenCommon (token, token1)
 import Parser.Classic.Gen.Lits (ValidLit(..))
 import Parser.Classic.Gen.Idents (ValidIdent(..), InvalidIdent(..))
 
 import Test.Tasty.QuickCheck
   ( Arbitrary, Gen
   , arbitrary, shrink
-  , oneof, resize, sized, suchThat
+  , elements, oneof, resize, sized, suchThat
   )
 
 newtype ValidExpr
@@ -47,6 +48,33 @@ validIdentToExpr (ValidIdent (s, n, shrunkenIdents))
 genIdentExpr :: Gen ValidExpr
 genIdentExpr = fmap validIdentToExpr arbitrary
 
+genExpr8 :: Gen ValidExpr
+genExpr8 = do
+  (o, c) <- elements prec8
+  so <- token1 o
+  (se, e', es) <- fmap validExpr $ genExpr' genExpr8
+  let e = c e'
+  return $ ValidExpr (so ++ se, e, es)
+
+genExpr7 :: Gen ValidExpr
+genExpr7 = genBinExpr genExpr7 genExpr8 prec7
+
+genExpr6 :: Gen ValidExpr
+genExpr6 = genBinExpr genExpr6 genExpr7 prec6
+
+genExpr5 :: Gen ValidExpr
+genExpr5 = genBinExpr genExpr5 genExpr6 prec5
+
+genBinExpr :: Gen ValidExpr -> Gen ValidExpr ->
+  [(String, Expr -> Expr -> Expr)] -> Gen ValidExpr
+genBinExpr left right prec = do
+  (se1, e1, es1) <- fmap validExpr $ genExpr' left
+  (o, c) <- elements prec
+  so <- token1 o
+  (se2, e2, es2) <- fmap validExpr $ genExpr' right
+  let e = c e1 e2
+  return $ ValidExpr (se1 ++ so ++ se2, e, es1 ++ es2)
+
 genParensExpr :: Gen ValidExpr
 genParensExpr = do
   so <- token "("
@@ -54,17 +82,19 @@ genParensExpr = do
   sc <- token ")"
   return $ ValidExpr (so ++ se ++ sc, e, es)
 
-genExpr :: Int -> Gen ValidExpr
-genExpr 0 = oneof
-  [ genIdentExpr
-  , genLitExpr
-  ]
-genExpr n = resize (n `div` 2) $ oneof
-  [ genParensExpr
-  ]
+genExpr' :: Gen ValidExpr -> Gen ValidExpr
+genExpr' g = sized $ \n ->
+  case n of
+    0 -> oneof [genIdentExpr, genLitExpr]
+    _ -> resize (n `div` 2) $ oneof [g, genParensExpr]
 
 instance Arbitrary ValidExpr where
-  arbitrary = sized genExpr
+  arbitrary = oneof $
+    [ genExpr' genExpr8
+    , genExpr' genExpr7
+    , genExpr' genExpr6
+    , genExpr' genExpr5
+    ]
 
   shrink (ValidExpr (_, _, es)) = es
 
