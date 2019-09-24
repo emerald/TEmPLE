91a522026cb2d26b749e31a500289485dcf4bee8
Oleks <oleks@oleks.info>
2019-09-24 21:21:55 +0200

    Test unavailable and failure blocks

---
+++ golden/Parser/Classic/DeclStats/unavailable-failure.m
@@ -0,0 +1,34 @@
+begin
+end
+
+begin
+  unavailable
+  end unavailable
+end
+
+begin
+  unavailable [ x ]
+  end unavailable
+end
+
+
+begin
+  unavailable
+    move x to y
+  end unavailable
+end
+
+begin
+  failure
+  end failure
+end
+
+begin
+  unavailable
+    move x to y
+  end unavailable
+
+  failure
+    move x to y
+  end failure
+end
