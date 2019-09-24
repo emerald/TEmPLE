9475c53ecea0a921773fe8f6ef6234d1217f8323
Oleks <oleks@oleks.info>
2019-09-24 19:26:40 +0200

    Test constants and variables inside objects

---
+++ golden/Parser/Classic/Objects/const-vars.m
@@ -0,0 +1,13 @@
+object x
+
+  const a <- 0
+  const b : integer <- 0
+  attached const c <- 0
+  attached const d : integer <- 0
+
+  var e : integer
+  var d : integer <- 0
+  attached var f : integer
+  attached var g : integer <- 0
+
+end x
