09e331318dc24d62d5b76df48dceaedd4dd3e877
Oleks <oleks@oleks.info>
2020-01-14 17:12:40 +0100

    Provide some error messages in case of failure

--- src/Parser/ClassicMegaparsec/TextLits.hs
+++ src/Parser/ClassicMegaparsec/TextLits.hs
@@ -24,7 +24,7 @@ import Util ( liftMaybe )
 import Data.Text ( Text )
 import Data.Void ( Void )
 import Text.Megaparsec
-  ( Parsec
+  ( Parsec    , (<?>)
   , anySingle , between , choice
   , count     , many    , satisfy
   , try
@@ -38,14 +38,15 @@ parseOctChar = satisfy isOctChar
 
 parseEscSeq :: Parser Char
 parseEscSeq = string "\\" *> choice
-  [ satisfy isAnyChar >>= liftMaybe . escSeqAny_to_C
+  [ try (satisfy isAnyChar >>= liftMaybe . escSeqAny_to_C)
+      <?> "a valid escape sequence (e.g., \\n, \\t)"
   , string "^" *> fmap escSeqUp_to_C anySingle
   , choice
     [ try $ count 3 parseOctChar
     , try $ count 2 parseOctChar
     ,       count 1 parseOctChar
-    ] >>= liftMaybe . escSeqOct_to_C
-  ]
+    ] >>= liftMaybe . escSeqOct_to_C <?> "1-3 octal digits"
+  ] <?> "a valid escape sequence (e.g., \\n, \\^J, \\012)"
 
 parseChar :: Parser Lit
 parseChar =
