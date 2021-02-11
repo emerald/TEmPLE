c25bbd0cae805548fcd205c7023d25093b15e53f
Oleks <oleks@oleks.info>
2021-02-11 16:48:08 +0100

    Use word combinator as in the ReadP variant

--- src/Parser/Classic/Megaparsec/Base.hs
+++ src/Parser/Classic/Megaparsec/Base.hs
@@ -1,5 +1,6 @@
 module Parser.Classic.Megaparsec.Base
   ( symbol
+  , word
   ) where
 
 import Control.Applicative ( empty )
@@ -7,6 +8,7 @@ import qualified Text.Megaparsec.Char.Lexer as L
   ( space, symbol )
 import qualified Text.Megaparsec.Char as C
   ( space1 )
+import Data.Foldable ( asum )
 
 import Parser.Classic.Megaparsec.Types ( Parser )
 
@@ -15,3 +17,6 @@ space = L.space C.space1 empty empty
 
 symbol :: String -> Parser String
 symbol = L.symbol space
+
+word :: [(String, a)] -> Parser a
+word = asum . map (\(w, a) -> symbol w *> return a)
--- src/Parser/Classic/Megaparsec/Lits.hs
+++ src/Parser/Classic/Megaparsec/Lits.hs
@@ -5,16 +5,16 @@ module Parser.Classic.Megaparsec.Lits
 import Control.Applicative ( (<|>) )
 
 import Ast ( Lit( LNil, LSelf ) )
-import Parser.Classic.Megaparsec.Base ( symbol )
+import Parser.Classic.Megaparsec.Base ( word )
 import Parser.Classic.Megaparsec.Types ( Parser )
 import qualified Parser.Classic.Words as W
   ( Literals( Nil, Self ) )
 
 parseNil :: Parser Lit
-parseNil = symbol (show W.Nil) *> return LNil
+parseNil = word [(show W.Nil, LNil)]
 
 parseSelf :: Parser Lit
-parseSelf = symbol (show W.Self) *> return LSelf
+parseSelf = word [(show W.Self, LSelf)]
 
 parseLit :: Parser Lit
 parseLit = parseNil <|> parseSelf
