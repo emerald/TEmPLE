ffd43fb8775819364c5b7f56109a76c52753052e
Oleks <oleks@oleks.info>
2021-02-15 20:38:33 +0100

    Add a fullParse, similar to the ReadP variant

--- src/Parser/Classic/Megaparsec/Base.hs
+++ src/Parser/Classic/Megaparsec/Base.hs
@@ -1,9 +1,13 @@
 module Parser.Classic.Megaparsec.Base
   ( symbol
   , word
+  , fullParse
   ) where
 
 import Control.Applicative ( empty )
+import Data.Void ( Void )
+import Text.Megaparsec.Error ( ParseErrorBundle )
+import Text.Megaparsec ( eof, parse )
 import qualified Text.Megaparsec.Char.Lexer as L
   ( space, symbol' )
 import qualified Text.Megaparsec.Char as C
@@ -20,3 +24,6 @@ symbol = L.symbol' space
 
 word :: [(String, a)] -> Parser a
 word = asum . map (\(w, a) -> symbol w *> return a)
+
+fullParse :: Parser a -> String -> Either (ParseErrorBundle String Void) a
+fullParse p s = parse (p <* eof) "" s
